6fda553d7f3b416caa478533c44f3b8cab582d75
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-09-23 21:44:27 +0530

    added dummy text

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,46 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Chat from '../images/chat.png'
 
 function Nav({ auth }) {
 
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleOpenClick = () => {
+        setIsOpen(true)
+    }
+
     const navItems = auth ? (
-        <ul className="nav-list right">
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                        <li>
-                            <Link to="/signup">Signup</Link>
-                        </li>
-                    </ul>
+        <ul className="nav-list right purple">
+             <div class="menu-icon close">
+                <i class="fas fa-times"></i>
+             </div>        
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+            <li>
+                <Link to="/signup">Signup</Link>
+            </li>
+        </ul>
     ) : (
-        <ul className="nav-list right">
-                        <li>
-                            <Link to="/create">Create Project</Link>
-                        </li>
-                        <li>
-                            <Link to="/profile">Profile</Link>
-                        </li>
-                        <li>
-                            <Link to="/logout">Logout</Link>
-                        </li>
-                        <li>
-                            <Link to="/logout">
-                                <div className="img">
-                                    <img src={Chat} alt=""/>
-                                </div>
-                            </Link>
-                        </li>
-                    </ul> 
+        <ul className={`nav-list right purple ${ isOpen && 'active'}`}>
+             <div class="menu-icon close">
+                <i class="fas fa-times"></i>
+             </div>
+            <li>
+                <Link to="/create">Create Project</Link>
+            </li>
+            <li>
+                <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+                <Link to="/logout">Logout</Link>
+            </li>
+            <li>
+                <Link to="/logout">
+                    <div className="img msg-bg">
+                        <img src={Chat} alt=""/>
+                    </div>
+                </Link>
+            </li>
+        </ul> 
     )
 
     return (
         <header>
-            <nav className="nav-wrapper grey lighten-1">
+            <nav className="nav-wrapper purple">
                 <div className="container">
                     <Link to="/" >
-                        <a href="#" className="brand-logo left orange-text">Devhub</a>
+                        <a href="#" className="brand-logo hide-on-med-and-down left orange-text">Devhub</a>
                     </Link>
+                    <Link to="/" >
+                        <a href="#" className="brand-logo hide-on-large-only center orange-text">Devhub</a>
+                    </Link>
+                    <div class="menu-icon open left" onClick={handleOpenClick}>
+                        <i class="fas fa-align-left"></i>
+                    </div>
+                        <Link to="/logout">
+                            <div className="img right msg-sm" style={{marginTop: '5px'}}>
+                                <img src={Chat} alt=""/>
+                            </div>
+                        </Link>
                     { navItems }
                 </div>
             </nav>
