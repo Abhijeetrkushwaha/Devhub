2341c5493a628f45e1513ac7995dc70c470cae49
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-09-24 08:45:53 +0530

    added dummy text

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,40 +10,54 @@ function Nav({ auth }) {
         setIsOpen(true)
     }
 
+    const handleCloseClick = () => {
+        setIsOpen(false)
+    }
+
     const navItems = auth ? (
-        <ul className="nav-list right purple">
-             <div class="menu-icon close">
+        <ul className={`nav-list right purple ${ isOpen && 'active'}`}>
+        <div class="menu-icon close" onClick={handleCloseClick}>
+           <i class="fas fa-times"></i>
+        </div>
+       <li>
+           <Link to="/create" onClick={() => setIsOpen(false)}>Create Project</Link>
+       </li>
+       <li>
+           <Link to="/profile" onClick={() => setIsOpen(false)}>Profile</Link>
+       </li>
+       <li>
+           <Link to="/logout" onClick={() => setIsOpen(false)}>logout</Link>
+       </li>
+       <li>
+           <Link to="/chat">
+               <div className="img msg-bg">
+                   <img src={Chat} alt=""/>
+               </div>
+           </Link>
+       </li>
+   </ul>
+    ) : (
+        <ul className={`nav-list right purple ${ isOpen && 'active'}`}>
+             <div class="menu-icon close" onClick={handleCloseClick}>
                 <i class="fas fa-times"></i>
              </div>        
             <li>
-                <Link to="/login">Login</Link>
+                <Link to="/login" onClick={() => setIsOpen(false)}>Login</Link>
             </li>
             <li>
-                <Link to="/signup">Signup</Link>
-            </li>
-        </ul>
-    ) : (
-        <ul className={`nav-list right purple ${ isOpen && 'active'}`}>
-             <div class="menu-icon close">
-                <i class="fas fa-times"></i>
-             </div>
-            <li>
-                <Link to="/create">Create Project</Link>
-            </li>
-            <li>
-                <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-                <Link to="/logout">logout</Link>
-            </li>
-            <li>
-                <Link to="/logout">
-                    <div className="img msg-bg">
-                        <img src={Chat} alt=""/>
-                    </div>
-                </Link>
+                <Link to="/signup" onClick={() => setIsOpen(false)}>Signup</Link>
             </li>
         </ul> 
+    )
+
+    const chatRoomAtSmall = auth ? (
+        <Link to="/chat">
+            <div className="img right msg-sm">
+                <img src={Chat} alt=""/>
+            </div>
+        </Link>
+    ) : (
+        null
     )
 
     return (
@@ -59,11 +73,7 @@ function Nav({ auth }) {
                     <div class="menu-icon open left" onClick={handleOpenClick}>
                         <i class="fas fa-align-left"></i>
                     </div>
-                        <Link to="/logout">
-                            <div className="img right msg-sm" style={{marginTop: '5px'}}>
-                                <img src={Chat} alt=""/>
-                            </div>
-                        </Link>
+                    { chatRoomAtSmall } 
                     { navItems }
                 </div>
             </nav>
