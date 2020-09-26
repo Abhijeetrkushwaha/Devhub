44ca11d30eb35d3d6dd3b2d2dd1d8f5d3d28b5c2
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-09-26 11:38:08 +0530

    added dummy text

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCPyixDkro2_eH2oSZgwQmMqpZM3-vCObQ",
-  authDomain: "instaclone-81899.firebaseapp.com",
-  databaseURL: "https://instaclone-81899.firebaseio.com",
-  projectId: "instaclone-81899",
-  storageBucket: "instaclone-81899.appspot.com",
-  messagingSenderId: "914657216205",
-  appId: "1:914657216205:web:7ab3fbf640e490f2d84b84",
-  measurementId: "G-SX6TQVY3GL"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDING_ID,
+  appId: process.env.REACT_APP_API_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 })
 
 const db = firebaseApp.firestore();
