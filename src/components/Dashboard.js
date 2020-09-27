597aee38a839dad801d951c6a098190ae5429a8e
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-09-27 20:20:03 +0530

    added welcome text

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Notification from './Notification';
 import ProjectList from './ProjectList';
 import { Redirect } from 'react-router-dom';
-// import { auth } from '../firebase';
+import { Link } from 'react-router-dom'
 
 function Dashboard({ user }) {
     // console.log(user && user.uid);
     const userIsLogin = !user ?   <Redirect to="/signup" /> : (
         <div className="dashboard">
+            <div className="welcome-greeting center">
+                <h5><span className="purple-text">Welcome </span>{user.displayName}</h5>
+                <Link to="/create" className="take-to-create">Create a project</Link>
+            </div>
             <div className="row">
                 <div className="col s12 m12 l6">
-                    <ProjectList />
                     <ProjectList />
                 </div>
                 <div className="col s12 m12 l5 offset-l1">
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import Chat from '../images/chatapp.png'
+import Chat from '../images/chatapp.png';
+import { db } from '../firebase'
 
 const ProjectList = () => {
-    return (
-        <div className="section">
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        db.collection('projects')
+        // .orderBy('timestamp', 'desc')
+        .onSnapshot(snapshot =>{
+            setProjects(snapshot.docs.map(doc => (
+              {project: doc.data(), id: doc.id}
+            )))
+        })
+      }, [] )
+
+    //   console.log(projects);
+
+      const projectLists = projects.map(({ project, id}) => {
+          return (
+            <div className="section" key={id}>
             <div className="card z-depth-1 project-summary">
                 <div className="user-info">
                     <div className="user-detail">
-                        <Avatar className="info__img" alt="Abhijeet" src='/img' />
-                        <p className="username">Abhijeet Kushwaha</p>
+                        <Avatar className="info__img" alt={project.username} src='/img' />
+                        <p className="username">{project.username}</p>
                     </div>
                     <p className="grey-text">24th September, 10am</p>
                 </div>
                 <div className="post__image">
                     <img src={Chat} alt="Technical issue or slow net speed"/> 
-            </div>
-                <div className="card-content project-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <button className="btn-small purple">View</button>
+                </div>
+                    <div className="card-content project-content grey-text text-darken-3">
+                        <span className="card-title">{project.title}</span>
+                        <button className="btn-small purple">View</button>
+                    </div>
                 </div>
             </div>
+          )
+      })
+
+    return (
+        <div>
+            {projectLists}
         </div>
     )
 }
